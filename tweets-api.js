d99057913c86ea639e0cdabf176441c7dbfaa824
v4rgas <juanvargas@uc.cl>
2023-05-18 11:55:47 -0400

    arreglada vista en telefonos

--- tweets-api.js
+++ tweets-api.js
@@ -6,11 +6,11 @@ const getTweets = async () => {
     const tweet_date = new Date(date * 1000).toLocaleString();
     mainContainer.innerHTML += `
     <div class="card mb-3">
-        <div class="card-body d-flex">
+        <div class="card-body d-inline-flex">
         <div style="width:5em;">
             <identicon-svg username="${user}"></identicon-svg>
         </div>
-        <div>
+        <div mx-auto>
             <h5 class="card-title">${user}</h5>
             <h6 class="card-subtitle text-muted">${tweet_date}</h6>
             <p class="card-text">${body}</p>
