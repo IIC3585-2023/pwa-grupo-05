3980d81c55bf8b148ac13f04251b6aaa39e41ba4
sebacarrasco <sebacarrasco@uc.cl>
2023-05-17 23:24:22 -0400

    change path to firebase sw2

--- firebase-config.js
+++ firebase-config.js
@@ -17,7 +17,7 @@ const messaging = getMessaging(app);
 
 const listen = async () => {
   try {
-    const register = await navigator.serviceWorker.register('./pwa-grupo-05/firebase-messaging-sw.js');
+    const register = await navigator.serviceWorker.register('./firebase-messaging-sw.js');
     const token = await getToken(messaging, {
       vapidKey: "BH_HP_crMNHblGKqgHcpWtpu3M76vv71jFBrHTmInrQShsUmIyGJU29lpzND50Z5XeiruZxNvXbC4Q5YC71b5dY",
       serviceWorkerRegistration: register
